b2633de757a85ffdd79ad41db37a5a08289983a9
Victoria Iascevschi <victoria.iascevschi@student.nhlstenden.com>
2026-03-29 13:01:13 +0200

    Fixed DemoPresentation class

--- src/main/java/io/DemoPresentation.java
+++ src/main/java/io/DemoPresentation.java
@@ -4,7 +4,7 @@ import main.java.businesslogic.Presentation;
 import main.java.businesslogic.Slide;
 import main.java.businesslogic.SlideItemFactory;
 
-/** A built in demo-presentation
+/** A built-in demo-presentation
  * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
  * @version 1.1 2002/12/17 Gert Florijn
  * @version 1.2 2003/11/19 Sylvia Stuurman
@@ -20,40 +20,41 @@ class DemoPresentation extends Accessor {
 		presentation.setTitle("Demo main.java.logic.Presentation");
 		Slide slide;
 		slide = new Slide();
+		SlideItemFactory factory = new SlideItemFactory();
 		slide.setTitle("main.java.app.JabberPoint");
-		slide.append(1, "The Java main.java.logic.Presentation Tool");
-		slide.append(2, "Copyright (c) 1996-2000: Ian Darwin");
-		slide.append(2, "Copyright (c) 2000-now:");
-		slide.append(2, "Gert Florijn andn Sylvia Stuurman");
-		slide.append(4, "Starting main.java.app.JabberPoint without a filename");
-		slide.append(4, "shows this presentation");
-		slide.append(1, "Navigate:");
-		slide.append(3, "Next slide: PgDn or Enter");
-		slide.append(3, "Previous slide: PgUp or up-arrow");
-		slide.append(3, "Quit: q or Q");
+		slide.append(factory.createSlideItem(1, "The Java main.java.logic.Presentation Tool"));
+		slide.append(factory.createSlideItem(2, "Copyright (c) 1996-2000: Ian Darwin"));
+		slide.append(factory.createSlideItem(2, "Copyright (c) 2000-now:"));
+		slide.append(factory.createSlideItem(2, "Gert Florijn andn Sylvia Stuurman"));
+		slide.append(factory.createSlideItem(4, "Starting main.java.app.JabberPoint without a filename"));
+		slide.append(factory.createSlideItem(4, "shows this presentation"));
+		slide.append(factory.createSlideItem(1, "Navigate:"));
+		slide.append(factory.createSlideItem(3, "Next slide: PgDn or Enter"));
+		slide.append(factory.createSlideItem(3, "Previous slide: PgUp or up-arrow"));
+		slide.append(factory.createSlideItem(3, "Quit: q or Q"));
 		presentation.append(slide);
 
 		slide = new Slide();
 		slide.setTitle("Demonstration of levels and stijlen");
-		slide.append(1, "Level 1");
-		slide.append(2, "Level 2");
-		slide.append(1, "Again level 1");
-		slide.append(1, "Level 1 has style number 1");
-		slide.append(2, "Level 2 has style number  2");
-		slide.append(3, "This is how level 3 looks like");
-		slide.append(4, "And this is level 4");
+		slide.append(factory.createSlideItem(1, "Level 1"));
+		slide.append(factory.createSlideItem(2, "Level 2"));
+		slide.append(factory.createSlideItem(1, "Again level 1"));
+		slide.append(factory.createSlideItem(1, "Level 1 has style number 1"));
+		slide.append(factory.createSlideItem(2, "Level 2 has style number  2"));
+		slide.append(factory.createSlideItem(3, "This is how level 3 looks like"));
+		slide.append(factory.createSlideItem(4, "And this is level 4"));
 		presentation.append(slide);
 
 		slide = new Slide();
 		slide.setTitle("The third slide");
-		slide.append(1, "To open a new presentation,");
-		slide.append(2, "use File->Open from the menu.");
-		slide.append(1, " ");
-		slide.append(1, "This is the end of the presentation.");
-		SlideItemFactory factory = new SlideItemFactory();
+		slide.append(factory.createSlideItem(1, "To open a new presentation,"));
+		slide.append(factory.createSlideItem(2, "use File->Open from the menu."));
+		slide.append(factory.createSlideItem(1, " "));
+		slide.append(factory.createSlideItem(1, "This is the end of the presentation."));
+		SlideItemFactory slideItemFactory = new SlideItemFactory();
 
-		slide.append(factory.createSlideItem("image", 1, "main.java.app.JabberPoint.jpg"));
-		slide.append(factory.createSlideItem("text", 1, "Some text"));
+		slide.append(slideItemFactory.createSlideItem("image", 1, "main.java.app.JabberPoint.jpg"));
+		slide.append(slideItemFactory.createSlideItem("text", 1, "Some text"));
 
 		presentation.append(slide);
 	}
