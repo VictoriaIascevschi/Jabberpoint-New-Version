50517f71aee6a3e8e6712efd998f9c60eaa4783e
Victoria Iascevschi <victoria.iascevschi@student.nhlstenden.com>
2026-03-29 19:43:21 +0200

    Restructured all the files

--- src/main/java/businesslogic/PresentationObserver.java
+++ src/main/java/businesslogic/PresentationObserver.java
@@ -1,9 +1,6 @@
 package main.java.businesslogic;
 
-import main.java.businesslogic.Presentation;
-import main.java.businesslogic.Slide;
-
 public interface PresentationObserver
 {
-    public void update(Presentation presentation, Slide data);
+    void update(Presentation presentation, Slide data);
 }
